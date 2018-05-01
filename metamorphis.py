cfc780f3ae31c0b92d8a66e471a08dbe6628d1d2
JackieW001 <jwoo@stuy.edu>
2018-04-30 21:02:36 -0400

    most_freq done

--- metamorphis.py
+++ metamorphis.py
@@ -5,16 +5,22 @@ story = story_raw.split()
 # Frequency of a single word
 def finder(word):
     count = len([x for x in story if x == word])
-    print "Finding word:",word,"Count:",count
+    #print "Finding word:",word,"Count:",count
+    return count
 
-finder("One")
-
+print "Word: ", "One", "Count: ", finder("One")
+   
 # Frequency of 2 words
 def phrase_finder(phrase):
     phrase = phrase.split()
     pairs = [[story[i], story[i+1]] for i in range(0, len(story) - 1)]
     count = len([x for x in pairs if x == phrase])
-    print "Finding:",phrase,"Found:",count
     return count
 
-phrase_finder('help produce')
+print "Phrase: ", "help produce", "Count: ", phrase_finder('help produce')
+
+def most_freq():
+    freq = [ [x,finder(x)] for x in story]
+    return reduce(lambda a,b: a if a[1] > b[1] else b, freq)
+
+print most_freq()
